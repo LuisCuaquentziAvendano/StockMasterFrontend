4a3319e0584d6511ad4f11cb72185951004060f1
Luis Antonio Cuaquentzi Avendaño <luis.cuaquentzi@gmail.com>
2025-04-06 13:24:19 -0600

    chore: server port fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'dist', 'stock-master-frontend', 'browser')));
 
