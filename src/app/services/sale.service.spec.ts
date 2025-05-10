85ad140c20b66c8f3e18c6490fdf30209c605809
carlosssfelixxx <carlosfs16@hotmail.com>
2025-05-09 20:19:55 -0700

    test: more tests

--- src/app/services/sale.service.spec.ts
+++ src/app/services/sale.service.spec.ts
@@ -1,16 +1,49 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { SaleService } from './sale.service';
+import { HttpService } from './http.service';
+import { AuthenticationService } from './authentication.service';
+import { environment } from '../../environments/environment';
+import { PurchasedProduct } from '../types/sale';
 
-// import { SaleService } from './sale.service';
+describe('SaleService', () => {
+  let service: SaleService;
 
-// describe('SaleService', () => {
-//   let service: SaleService;
+  const mockHttpService = {
+    post: jasmine.createSpy('post'),
+    get: jasmine.createSpy('get'),
+  };
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(SaleService);
-//   });
+  const mockAuthService = {
+    getToken: jasmine.createSpy('getToken').and.returnValue('TOKEN'),
+  };
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: HttpService, useValue: mockHttpService },
+        { provide: AuthenticationService, useValue: mockAuthService },
+      ],
+    });
+    service = TestBed.inject(SaleService);
+  });
+
+  it('should make a purchase', () => {
+    const products: PurchasedProduct[] = [
+      { product_id: '1', amount: "2", price: "100" },
+    ];
+    service.makePurchase('inv-123', 'John Doe', 'pm-456', products);
+    expect(mockAuthService.getToken).toHaveBeenCalled();
+    expect(mockHttpService.post).toHaveBeenCalledWith({
+      url: `${environment.BASE_URL}/sales/makePurchase`,
+      headers: {
+        authorization: 'TOKEN',
+        inventory: 'inv-123',
+      },
+      body: {
+        customer: 'John Doe',
+        paymentMethodId: 'pm-456',
+        products,
+      },
+    });
+  });
+});
